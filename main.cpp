b649e7706363573259aaf537bd3caf01b6dd41e1
Cluley <oo43@yandex.ru>
2023-08-02 22:21:45 +0700

    shape class interface improvements

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <random>
 
 #include "shape.h"
 #include "transform.h"
 
 int main() {
-	Shape cube(2, 6, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 4, 2, 3, 4, 5, 6);
-	std::cout << "Площадь: " << cube.square << "\nОбъем: " << cube.volume << std::endl;
+	std::multimap<char, std::vector<int>> coord;
+	coord.emplace('x', std::vector<int> {2, 4, 6, 8, 10, 12, 14, 16});
+	coord.emplace('y', std::vector<int> {3, 5, 7, 9, 11, 13, 15, 17});
+	coord.emplace('z', std::vector<int> {18, 19, 20, 21, 22, 23, 24, 25});
+
+	Shape cube(Shape::TypeOne::cube, coord);
+	std::cout << "Площадь куба: " << cube.square << "\nОбъем куба: " << cube.volume << std::endl;
+
+	Shape cylinder(Shape::TypeTwo::cylinder, coord, 15, 30);
+	std::cout << "\nПлощадь цилиндра: " << cylinder.square << "\nОбъем цилиндра: " << cylinder.volume << std::endl;
 
 	transform cube_t(cube);
 	cube_t.scale(2);
 
-	std::cout << "Площадь: " << cube.square << "\nОбъем: " << cube.volume << std::endl;
+	std::cout << "\nПлощадь куба после скалирования: " << cube.square << "\nОбъем куба после скалирования: " << cube.volume << std::endl;
 
 	return 0;
 }
--- shape.cpp
+++ shape.cpp
@@ -3,100 +3,58 @@
 #include"shape.h"
 #include<cmath>
 
-Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
-{
-	type = static_cast<Type>(_type);
-	// заполн¤ем координаты фигуры
-	switch (type)
-	{
-	case Type::line:
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		break;
-	case Type::sqr:
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		x3 = _x3; y3 = _y3;
-		x4 = _x4; y4 = _y4;
-		break;
-	case Type::cube:
-		x1 = _x1; y1 = _y1; z1 = _z1;
-		x2 = _x2; y2 = _y2; z2 = _z2;
-		x3 = _x3; y3 = _y3; z3 = _z3;
-		x4 = _x4; y4 = _y4; z4 = _z4;
-		x5 = _x5; y5 = _y5; z5 = _z5;
-		x6 = _x6; y6 = _y6; z6 = _z6;
-		x7 = _x7; y7 = _y7; z7 = _z7;
-		x8 = _x8; y8 = _y8; z8 = _z8;
-		break;
-	default:
-		break;
-	}
+Shape::Shape(TypeOne _type, std::multimap<char, std::vector<int>>& _coord) {
+	type = _type;
+	coord = _coord;
 
-	// стороны фигуры
-	int a = abs(x1 - x2);
-	int b = abs(y1 - y2);
-	int c = abs(z1 - z2);
-	// считаем площадь фигуры
-	switch (type)
+	int a = abs(coord.find('x')->second[0] - coord.find('x')->second[1]);
+	int b = abs(coord.find('y')->second[0] - coord.find('y')->second[1]);
+	int c = abs(coord.find('z')->second[0] - coord.find('z')->second[1]);
+
+	switch (std::get<TypeOne>(type))
 	{
-	case Type::line:
+	case TypeOne::line:
 		square = 0;
 		break;
-	case Type::sqr:
+	case TypeOne::sqr:
 		square = a * b;
 		break;
-	case Type::cube:
+	case TypeOne::cube:
 		square = 2 * a * b + 2 * a * c + 2 * b * c;
 		break;
 	default:
 		break;
 	}
 
-	// считаем объем фигуры
-	switch (type)
+	switch (std::get<TypeOne>(type))
 	{
-	case Type::line:
+	case TypeOne::line:
 		volume = 0;
 		break;
-	case Type::sqr:
+	case TypeOne::sqr:
 		volume = 0;
 		break;
-	case Type::cube:
+	case TypeOne::cube:
 		volume = a * b * c;
 		break;
 	default:
 		break;
 	}
-
 }
 
-Shape::Shape(int _type, int _x1, int _y1, double R, double H)
-{
-	type = static_cast<Type>(_type);
-	// заполн¤ем координаты фигуры
-	switch (type)
-	{
-	case Type::circle:
-		x1 = _x1; y1 = _y1;
-		radius = R;
-		break;
-	case Type::cylinder:
-		x1 = _x1; y1 = _y1;
-		radius = R;
-		height = H;
-		break;
-	default:
-		break;
-	}
+Shape::Shape(TypeTwo _type, std::multimap<char, std::vector<int>>& _coord, double R, double H){
+	type = _type;
+	coord = _coord;
+	radius = R;
+	height = H;
 
 	// считаем площадь фигуры
-	switch (type)
+	switch (std::get<TypeTwo>(type))
 	{
-	case Type::circle:
+	case TypeTwo::circle:
 		square = M_PI * R * R;
 		break;
-	case Type::cylinder:
+	case TypeTwo::cylinder:
 		square = M_PI * R * R + 2 * R * height;
 		break;
 	default:
@@ -104,16 +62,15 @@ Shape::Shape(int _type, int _x1, int _y1, double R, double H)
 	}
 
 	// считаем объем фигуры
-	switch (type)
+	switch (std::get<TypeTwo>(type))
 	{
-	case Type::circle:
+	case TypeTwo::circle:
 		volume = 0;
 		break;
-	case Type::cylinder:
+	case TypeTwo::cylinder:
 		volume = M_PI * R * R * height;
 		break;
 	default:
 		break;
 	}
-
 }
